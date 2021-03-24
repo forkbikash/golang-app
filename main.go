789246701794c256eb804f5ecb76f19f63f5a127
Bikash Mishra <mishra.bikash002@gmail.com>
2021-03-24 21:24:54 +0530

    AuthRequired middleware

--- main.go
+++ main.go
@@ -28,8 +28,8 @@ func main() {
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 
 	r := mux.NewRouter()
-	r.HandleFunc("/", indexGetHandler).Methods("GET")
-	r.HandleFunc("/", indexPostHandler).Methods("POST")
+	r.HandleFunc("/", AuthRequired(indexGetHandler)).Methods("GET")
+	r.HandleFunc("/", AuthRequired(indexPostHandler)).Methods("POST")
 	r.HandleFunc("/login", loginGetHandler).Methods("GET")
 	r.HandleFunc("/login", loginPostHandler).Methods("POST")
 	r.HandleFunc("/register", registerGetHandler).Methods("GET")
@@ -47,13 +47,20 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
-func indexGetHandler(w http.ResponseWriter, r *http.Request) {
-	session, _ := store.Get(r, "session")
-	_, ok := session.Values["username"]
-	if !ok {
-		http.Redirect(w, r, "/login", http.StatusFound)
-		return
+// AuthRequired is a middleware
+func AuthRequired(handler http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session, _ := store.Get(r, "session")
+		_, ok := session.Values["username"]
+		if !ok {
+			http.Redirect(w, r, "/login", http.StatusFound)
+			return
+		}
+		handler.ServeHTTP(w, r)
 	}
+}
+
+func indexGetHandler(w http.ResponseWriter, r *http.Request) {
 
 	// getting data from redis server
 	comments, err := client.LRange("comments", 0, 10).Result()
